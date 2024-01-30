5055459ee4ca3a36c8daf5a4ec0cc9c2ae985505
Valeria Koss <valery_kosss@MacBook-Pro-Valeria.local>
2024-01-30 16:39:56 +0300

    travelist

---
+++ classics/script.js
@@ -0,0 +1,18 @@
+var faq = document.getElementsByClassName("faq-question");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        //document.getElementsByClassName("contacts-column").style.display = "none";
+        // document.getElementsByClassName("contacts-column").style.display = "none"; // убрвть название конакты
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
